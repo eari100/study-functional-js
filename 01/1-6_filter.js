6ee69545408ac0842c50afaa18c94c12be514758
Jaewook Choi <eari100@naver.com>
2024-02-26 00:13:17 +0900

    filter

---
+++ 01/1-6_filter.js
@@ -0,0 +1,10 @@
+function filter(list, predicate) {
+    const new_list = []
+
+    for(let i=0,len=list.length;i<len;i++) {
+        if(predicate(list[i])) new_list.push(list[i])
+    }
+    return new_list
+}
+
+module.exports = { filter }
