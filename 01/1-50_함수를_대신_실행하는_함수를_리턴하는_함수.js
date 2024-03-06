da3a67aa17144cf150ecfdf538a6ace30ff3c271
Jaewook Choi <eari100@naver.com>
2024-03-06 22:45:35 +0900

    함수를 대신 실행하는 함수를 리턴하는 함수

---
+++ 01/1-50_함수를_대신_실행하는_함수를_리턴하는_함수.js
@@ -0,0 +1,9 @@
+const callWith = val1 => (val2, func) => func(val1, val2)
+const add = (a, b) => a + b
+const sub = (a, b) => a - b
+
+const callWith10 = callWith(10)
+console.log(callWith10(20, add))
+
+const callWith5 = callWith(5)
+console.log(callWith5(5, sub))
