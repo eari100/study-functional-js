9cf47c7e20c35ccb4710cb7b52e863729d1b7d1a
Jaewook Choi <eari100@naver.com>
2024-03-06 23:21:45 +0900

    underscore.js 활용

--- 01/1-50_함수를_대신_실행하는_함수를_리턴하는_함수.js
+++ 01/1-50_함수를_대신_실행하는_함수를_리턴하는_함수.js
@@ -1,3 +1,6 @@
+import _ from 'underscore'
+
+
 const callWith = val1 => (val2, func) => func(val1, val2)
 const add = (a, b) => a + b
 const sub = (a, b) => a - b
@@ -11,3 +14,23 @@ console.log(callWith5(5, sub))
 // 괄호 두번
 callWith(30)(20, add)
 callWith(20)(20, sub)
+
+console.log(callWith([1, 2, 3])(v => v * 10, _.map))
+
+_.get = (list, idx) => list[idx]
+
+const callWithUsers = callWith([
+    { id: 1, name: 'HA', age: 25 },
+    { id: 2, name: 'PJ', age: 28 },
+    { id: 3, name: 'JE', age: 27 }
+])
+
+console.log(callWithUsers(2, _.get))
+
+console.log(callWithUsers((user) => user.age > 25, _.find))
+
+console.log(callWithUsers((user) => user.age > 25, _.filter))
+
+console.log(callWithUsers((user) => user.age > 25, _.some))
+
+console.log(callWithUsers((user) => user.age > 25, _.every))
