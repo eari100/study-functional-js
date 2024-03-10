4ed6a92c86b481c5a705989e063488f4be8886ea
Jaewook Choi <eari100@naver.com>
2024-03-10 21:24:41 +0900

    존 레식의 partial의 주의점

--- 01/1-54_partial.js
+++ 01/1-54_partial.js
@@ -22,3 +22,23 @@ ac('a', 'c')
 // 2. 초기에 인자 갯수 미리 채워놓아야한다 (아래 코드 참조)
 var ac2 = abc.partial(undefined, 'b')
 ac2('a', 'c') // a b undefined
+
+// 버그를 일으킬 수 있는 경우
+function add() {
+    var result = 0
+    for(var i=0;i<arguments.length;i++)
+        result += arguments[i]
+
+    return result
+}
+
+console.log('add', add(1, 2, 3, 4, 5))
+
+var add2 = add.partial(undefined, 2)
+console.log('add2', add2(1, 2, 4, 5))
+
+var add3 = add.partial(undefined, undefined, 3, undefined, undefined)
+console.log('add3', add3(1, 2, 4, 5))
+
+console.log('bug1', add3(50, 50, 50, 50))
+console.log('bug2', add3(100, 100, 100, 100))
