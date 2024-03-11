222a9ccaf6048406ae6e91fc5f9a0b85db5f644d
Jaewook Choi <eari100@naver.com>
2024-03-11 23:28:03 +0900

    배열 크기 늘리기

---
+++ 02/2-6_함수를_객체로_사용.js
@@ -0,0 +1,7 @@
+// 함수도 객체이다
+function obj8() {}
+obj8.a = 1
+obj8.b = 2
+
+console.log(obj8.a)
+console.log(obj8.b)
---
+++ 02/2-9_배열에_숫자로_key_사용하기.js
@@ -0,0 +1,7 @@
+// push와 동일
+var obj11 = []
+obj11[0] = 1
+obj11[1] = 2
+console.log(obj11)
+
+console.log(obj11.length)
